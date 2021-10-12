7edf1aebb811bfb3608b95a98e94827fc73f168d
Panita Tharaphoom <panita.tharaphom@gmail.com>
2021-10-12 22:18:56 +0700

    StreamBuilder in EighthPage

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,7 @@
 // import 'dart:js';
 
 import 'package:first_app/models/first_form_model.dart';
+import 'package:first_app/pages/eighth_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -43,7 +44,8 @@ class MyApp extends StatelessWidget {
           '/4': (context) => FourthPage(),
           '/5': (context) => fifthPage(),
           '/6': (context) => SixthPage(),
-          '/7': (context) => SeventhPage()
+          '/7': (context) => SeventhPage(),
+          '/8': (context) => EighthPage()
         });
   }
 }
